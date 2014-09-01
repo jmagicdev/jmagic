4ca474c284f14e174a9f73928e409c0a901776bd
RulesGuru <jmagicdev+RulesGuru@gmail.com>
2014-09-01 16:17:04 -0400

    CreateFromJson supports creating cards from a specific list (give --deck on the command line)

--- development/src/main/java/org/rnd/jmagic/util/CreateFromJson.java
+++ development/src/main/java/org/rnd/jmagic/util/CreateFromJson.java
@@ -10,11 +10,52 @@ public class CreateFromJson
 {
 	public static void main(String args[]) throws java.io.IOException, URISyntaxException
 	{
+		boolean deck = false;
+		if(args.length > 0)
+		{
+			if(args[0].equals("--deck"))
+				deck = true;
+			else
+			{
+				System.out.println("May specify --deck to list individual cards; otherwise give no cmdline params to be asked for an expansion.");
+				return;
+			}
+		}
+
 		java.net.URL jsonLocation = CreateFromJson.class.getResource("AllSets-x.json");
 		if(null == jsonLocation)
 			jsonLocation = new java.net.URL("http://mtgjson.com/json/AllSets-x.json");
 		JsonReader jsonReader = Json.createReader(jsonLocation.openStream());
 		JsonObject root = jsonReader.readObject();
+
+		if(deck)
+		{
+			System.out.println("Type the name of each card you want, one per line, then give end-of-file (Ctrl-Z on windows)");
+			Set<String> cardNames = new HashSet<String>();
+			java.io.BufferedReader inputReader = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+			while(true)
+			{
+				String line = inputReader.readLine();
+				if(line == null)
+					break;
+				cardNames.add(line.trim());
+			}
+
+			JsonArrayBuilder cardsToWrite = Json.createArrayBuilder();
+			for(JsonValue expansion: root.values())
+				for(JsonValue cardValue: ((JsonObject)expansion).getJsonArray("cards"))
+				{
+					JsonObject card = (JsonObject)cardValue;
+					if(cardNames.contains(card.getString("name")))
+						cardsToWrite.add(card);
+				}
+
+			System.out.println("Writing card shells...");
+			cardsToWrite.build().forEach(new JsonConsumer(root));
+			return;
+		}
+
+		// --deck not given, process an expansion
 
 		List<String> expansions = new LinkedList<String>(root.keySet());
 
@@ -45,6 +86,7 @@ public class CreateFromJson
 		JsonObject expansionDetails = root.getJsonObject(expansion);
 		JsonArray cards = expansionDetails.getJsonArray("cards");
 
+		System.out.println("Writing card shells...");
 		cards.forEach(new JsonConsumer(root));
 	}
 
