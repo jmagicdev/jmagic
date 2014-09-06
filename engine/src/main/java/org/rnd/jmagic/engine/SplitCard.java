a60e239b1df6a943f7b10c23e338db1a941c250d
Kamikaze <jmagicdev+kamikaze@gmail.com>
2014-09-06 17:32:22 -0400

    add initial splitcard base implementation

--- engine/src/main/java/org/rnd/jmagic/engine/SplitCard.java
+++ engine/src/main/java/org/rnd/jmagic/engine/SplitCard.java
@@ -2,8 +2,15 @@ package org.rnd.jmagic.engine;
 
 public class SplitCard extends Card
 {
-	public SplitCard(GameState state)
+	public SplitCard(GameState state, Class<? extends Card> left, Class<? extends Card> right)
 	{
 		super(state);
+
+		this.characteristics = new Characteristics[] {//
+		Characteristics.createFromClass(state.game, left, this, false), // left
+		Characteristics.createFromClass(state.game, right, this, false) // right;
+		};
+
+		this.setName(java.util.Arrays.stream(this.characteristics).map(t -> t.name).reduce((l, r) -> l + " // " + r).orElse(""));
 	}
 }
