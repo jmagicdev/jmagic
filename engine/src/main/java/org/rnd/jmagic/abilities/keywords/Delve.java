f3be57c27c28ed7db44c0beafcc3889becb1e3b3
RulesGuru <jmagicdev+RulesGuru@gmail.com>
2014-09-06 00:55:37 -0400

    Updated Delve (apparently we had it already)

--- engine/src/main/java/org/rnd/jmagic/abilities/keywords/Delve.java
+++ engine/src/main/java/org/rnd/jmagic/abilities/keywords/Delve.java
@@ -1,6 +1,7 @@
 package org.rnd.jmagic.abilities.keywords;
 
 import static org.rnd.jmagic.Convenience.*;
+
 import org.rnd.jmagic.engine.*;
 import org.rnd.jmagic.engine.generators.*;
 
@@ -20,108 +21,53 @@ public final class Delve extends Keyword
 		return java.util.Collections.<StaticAbility>singletonList(new DelveAbility(this.state));
 	}
 
-	/**
-	 * TODO : This is basically just a generic additional cost; is it really
-	 * necessary to have a custom effect type? The only reason it was written
-	 * that way is that at the time it was written, no "generic" additional cost
-	 * effect type existed (only one for additional mana costs).
-	 * 
-	 * @eparam OBJECT: The object with delve.
-	 */
-	public static final ContinuousEffectType DELVE_ADDITIONAL_COST = new ContinuousEffectType("DELVE_ADDITIONAL_COST")
-	{
-		@Override
-		public Parameter affects()
-		{
-			return Parameter.OBJECT;
-		}
-
-		@Override
-		public void apply(GameState state, ContinuousEffect effect, java.util.Map<Parameter, Set> parameters)
-		{
-			EventFactory exile = new EventFactory(EventType.EXILE_CHOICE, "Exile any number of cards from your graveyard");
-			exile.parameters.put(EventType.Parameter.CAUSE, This.instance());
-			exile.parameters.put(EventType.Parameter.PLAYER, You.instance());
-			exile.parameters.put(EventType.Parameter.OBJECT, InZone.instance(GraveyardOf.instance(You.instance())));
-			exile.parameters.put(EventType.Parameter.NUMBER, Between.instance(0, null));
-
-			GameObject object = parameters.get(Parameter.OBJECT).getOne(GameObject.class);
-			object.optionalAdditionalCosts.add(new CostCollection(COST_TYPE, exile));
-		}
-
-		@Override
-		public Layer layer()
-		{
-			return Layer.RULE_CHANGE;
-		}
-	};
-
-	/**
-	 * @eparam OBJECT: The object with convoke.
-	 */
-	public static final ContinuousEffectType DELVE_REDUCED_COST = new ContinuousEffectType("DELVE_REDUCED_COST")
-	{
-		@Override
-		public Parameter affects()
-		{
-			return Parameter.OBJECT;
-		}
-
-		@Override
-		public void apply(GameState state, ContinuousEffect effect, java.util.Map<Parameter, Set> parameters)
-		{
-			GameObject object = parameters.get(Parameter.OBJECT).getOne(GameObject.class);
-
-			for(CostCollection cost: object.getOptionalAdditionalCostsChosen())
-			{
-				if(cost.type.equals(COST_TYPE))
-				{
-					EventFactory exileCostFactory = cost.events.iterator().next();
-					Event exileCost = object.getCostGenerated(state, exileCostFactory);
-
-					// If it hasn't created an event, the spell either wasn't
-					// convoked or hasn't been cast yet.
-					if(exileCost == null)
-						continue;
-
-					// Technically, delve's rules text says "each card
-					// exiled this way"... but at the time that cost reductions
-					// are applied, there are no creatures tapped, just
-					// creatures chosen.
-					Set chosenCards = exileCost.getChoices(object.getController(state));
-					ManaPool costReduction = new ManaPool("(" + chosenCards.size() + ")");
-
-					// TODO : 702.63b Multiple instances of delve on the same
-					// spell are redundant.
-					// If a spell has multiple instances of delve, this apply
-					// method will be called for each of them.
-					state.manaCostReductions.put(new Set(object), costReduction);
-				}
-			}
-		}
-
-		@Override
-		public Layer layer()
-		{
-			return Layer.RULE_CHANGE;
-		}
-	};
-
 	public static final class DelveAbility extends StaticAbility
 	{
 		public DelveAbility(GameState state)
 		{
-			super(state, "You may exile any number of cards from your graveyard as you cast this spell. It costs (1) less to cast for each card exiled this way.");
+			super(state, "Each card you exile from your graveyard while casting this spell pays for (1).");
 
-			ContinuousEffect.Part additionalCost = new ContinuousEffect.Part(DELVE_ADDITIONAL_COST);
-			additionalCost.parameters.put(ContinuousEffectType.Parameter.OBJECT, This.instance());
-			this.addEffectPart(additionalCost);
-
-			ContinuousEffect.Part costReduction = new ContinuousEffect.Part(DELVE_REDUCED_COST);
-			costReduction.parameters.put(ContinuousEffectType.Parameter.OBJECT, This.instance());
-			this.addEffectPart(costReduction);
+			ContinuousEffect.Part part = new ContinuousEffect.Part(ContinuousEffectType.ALTERNATE_PAYMENT);
+			part.parameters.put(ContinuousEffectType.Parameter.COST, Identity.instance(DELVE_PAYMENT));
+			part.parameters.put(ContinuousEffectType.Parameter.OBJECT, This.instance());
+			this.addEffectPart(part);
 
 			this.canApply = THIS_IS_ON_THE_STACK;
+		}
+	}
+
+	public static PlayerInterface.ChooseReason DELVE_REASON = new PlayerInterface.ChooseReason("Delve", "Exile cards from your graveyard to pay generic mana with delve.", true);
+
+	private static AlternateManaPayment DELVE_PAYMENT = new DelvePayment();
+
+	private static final class DelvePayment extends AlternateManaPayment
+	{
+		@Override
+		public int hashCode()
+		{
+			return 1;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			return obj instanceof DelvePayment;
+		}
+
+		@Override
+		public void pay(ManaPool cost, GameObject source)
+		{
+			int quantity = cost.stream().mapToInt(m -> m.colorless).sum();
+			EventFactory exile = new EventFactory(EventType.EXILE_CHOICE, "Exile up to " + quantity + " cards from your graveyard.");
+			exile.parameters.put(EventType.Parameter.CAUSE, This.instance());
+			exile.parameters.put(EventType.Parameter.NUMBER, Between.instance(0, quantity));
+			exile.parameters.put(EventType.Parameter.OBJECT, InZone.instance(GraveyardOf.instance(You.instance())));
+			exile.parameters.put(EventType.Parameter.PLAYER, You.instance());
+			Event exileEvent = exile.createEvent(source.game, source);
+			exileEvent.perform(null, true);
+
+			int exiled = exileEvent.getChoices(source.getController(source.game.actualState)).size();
+			cost.reduce(new ManaPool("" + exiled));
 		}
 	}
 }
