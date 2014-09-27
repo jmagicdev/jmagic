34518df610fb81d0b9e2cfb761b2f2a449197e5a
RulesGuru <jmagicdev+RulesGuru@gmail.com>
2014-09-27 14:27:54 -0400

    Fixed Lens of Clarity not looking at morphs

--- cards/src/main/java/org/rnd/jmagic/cards/LensofClarity.java
+++ cards/src/main/java/org/rnd/jmagic/cards/LensofClarity.java
@@ -15,17 +15,13 @@ public final class LensofClarity extends Card
 		{
 			super(state, "You may look at the top card of your library and at face-down creatures you don't control.");
 
-			ContinuousEffect.Part lookAtTop = new ContinuousEffect.Part(ContinuousEffectType.LOOK);
-			lookAtTop.parameters.put(ContinuousEffectType.Parameter.PLAYER, You.instance());
-			lookAtTop.parameters.put(ContinuousEffectType.Parameter.OBJECT, TopCards.instance(1, LibraryOf.instance(You.instance())));
-			this.addEffectPart(lookAtTop);
-
+			SetGenerator top = TopCards.instance(1, LibraryOf.instance(You.instance()));
 			SetGenerator morphs = Intersect.instance(FaceDown.instance(), CreaturePermanents.instance());
 			SetGenerator otherPlayersMorphs = RelativeComplement.instance(morphs, ControlledBy.instance(You.instance()));
 
 			ContinuousEffect.Part lookAtMorphs = new ContinuousEffect.Part(ContinuousEffectType.LOOK_PHYSICALLY);
 			lookAtMorphs.parameters.put(ContinuousEffectType.Parameter.PLAYER, You.instance());
-			lookAtMorphs.parameters.put(ContinuousEffectType.Parameter.OBJECT, otherPlayersMorphs);
+			lookAtMorphs.parameters.put(ContinuousEffectType.Parameter.OBJECT, Union.instance(top, otherPlayersMorphs));
 			this.addEffectPart(lookAtMorphs);
 		}
 	}
