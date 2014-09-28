b3e708f4abd76f59c30e2846484c2b38e4f2a600
RulesGuru <jmagicdev+RulesGuru@gmail.com>
2014-09-28 01:55:00 -0400

    Removing unused import

--- cards/src/main/java/org/rnd/jmagic/cards/MurderofCrows.java
+++ cards/src/main/java/org/rnd/jmagic/cards/MurderofCrows.java
@@ -3,7 +3,6 @@ package org.rnd.jmagic.cards;
 import static org.rnd.jmagic.Convenience.*;
 
 import org.rnd.jmagic.engine.*;
-import org.rnd.jmagic.engine.generators.*;
 
 @Name("Murder of Crows")
 @Types({Type.CREATURE})
